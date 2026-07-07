25712f502bc80a096bf3eb338956cb98542aedd7
Carlos Puente <accpmurillo233@gmail.com>
2026-07-07 06:21:03 -0600

    feat: implement category model

--- InventoryApi/Models/Product.cs
+++ InventoryApi/Models/Product.cs
@@ -23,17 +23,4 @@ namespace InventoryApi.Models
             return this;
         }
     }
-
-    public record  Category(int Id, string Name)
-    {
-
-        public async Task<string> GetDescriptionAsync()
-        {
-            await Task.Delay(500); ///simulando una consulta lenta a la BD
-            return $"Categoria: {Name}";
-         //   `Categoría: ${ name}` JavaScript
-         //   String.format()   Java
-        }
-
-    }
 }
