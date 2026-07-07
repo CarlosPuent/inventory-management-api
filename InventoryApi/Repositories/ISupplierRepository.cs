311b8ae69bfac78422bfba2cb7c09f638d65d84d
Carlos Puente <accpmurillo233@gmail.com>
2026-07-07 14:45:06 -0600

    feat: add Update and Delete to SupplierRepository, Service and Controller

--- InventoryApi/Repositories/ISupplierRepository.cs
+++ InventoryApi/Repositories/ISupplierRepository.cs
@@ -7,5 +7,7 @@ namespace InventoryApi.Repositories
         Task<List<Supplier>> GetAllAsync();
         Task<Supplier?> GetByIdAsync(int id);
         Task<Supplier> AddAsync(Supplier supplier);
+        Task<Supplier?> UpdateAsync(int id, Supplier supplier);
+        Task<bool> DeleteAsync(int id);
     }
 }
--- InventoryApi/Services/ISupplierService.cs
+++ InventoryApi/Services/ISupplierService.cs
@@ -4,9 +4,10 @@ namespace InventoryApi.Services
 {
     public interface ISupplierService
     {
-
         Task<List<Supplier>> GetAllSuppliersAsync();
         Task<Supplier?> GetSupplierByIdAsync(int id);
         Task<Supplier> CreateSupplierAsync(Supplier supplier);
+        Task<Supplier?> UpdateSupplierAsync(int id, Supplier supplier);
+        Task<bool> DeleteSupplierAsync(int id);
     }
 }
